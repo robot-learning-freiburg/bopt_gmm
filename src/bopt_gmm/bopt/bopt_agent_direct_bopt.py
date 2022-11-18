d562fd0b663f35a8ad9a5c54548aebfbe9845b9f
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2022-11-18 12:29:05 +0100

    Added missing force normalization to dbopt agent

--- src/bopt_gmm/bopt/bopt_agent_direct_bopt.py
+++ src/bopt_gmm/bopt/bopt_agent_direct_bopt.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
 
 from .bopt_agent_base import BOPTGMMAgentBase, \
-                             BOPTAgentConfig
+                             BOPTAgentConfig, \
+                             no_op
 
 @dataclass
 class BOPTAgentGMMConfig(BOPTAgentConfig):
     base_accuracy : float = 0.3
 
 class BOPTGMMAgent(BOPTGMMAgentBase):
-    def __init__(self, base_gmm, config : BOPTAgentGMMConfig, logger=None):
-        super().__init__(base_gmm, config, logger)
+    def __init__(self, base_gmm, config : BOPTAgentGMMConfig, obs_transform=no_op, logger=None):
+        super().__init__(base_gmm, config, obs_transform, logger)
 
         self.init_optimizer(config.base_accuracy)
