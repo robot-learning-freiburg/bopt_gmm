598803acbf9fbbf74c8e1a045e24af1983425f9c
Adrian Roefer <aroefer@uni-freiburg.de>
2023-01-13 17:04:29 +0100

    Fixed faulty PDF in GMMs and added conditional PDF

---
+++ scripts/gmm_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from bopt_gmm.gmm import GMM
+
+
+if __name__=='__main__':
+    class GMM3DPredictZ(GMM):
+        def __init__(self, priors, means, cvar):
+            super().__init__(priors=priors, means=means, cvar=cvar)
+
+        def predict(self, xy):
+            return super().predict(xy, (0, 1))
+
+        def predictX(self, yz):
+            return super().predict(yz, (1, 2))
+
+    g1 = GMM(priors=np.array([1, 1, 1]), 
+             means=np.array([[1, 2, 3]]).T, 
+             cvar=np.array([[[0.1]]]*3))
+    w1 = g1.get_weights(np.array([[1, 2, 3]]).T)
+    print(f'Weights for 1, 2, 3:\n{w1}')
+
+    g2 = GMM3DPredictZ(priors=np.array([1,  1,  1]),
+                       means=np.array([[1,  1,  1],
+                                       [4,  5, -2],
+                                       [9, -2,  4]]),
+                       cvar=np.stack([np.eye(3)]*3, axis=0))
+    
+    w2 = g2.get_weights(np.array([[1,  1,  1],
+                                  [4,  5, -2],
+                                  [9, -2,  4]]))
+    print(f'Weights for 3d case:\n{w2}')
+
+    w3 = g2.get_weights(np.array([[1,  1],
+                                  [4,  5],
+                                  [9, -2]]), dims=(0, 1))
+    print(f'Weights for 3d case without Z:\n{w3}')
+
+    w4 = g2.get_weights(np.array([[6.5, 1.5, 1]]))
+    print(f'3d point perfectly between 2 and 3:\n{w4}')
+
+    p1 = g2.predict(np.array([[4, 5]]))
+    print(f'Prediction for Z at (4, 5): {p1}')
+    print(f'Prediction for Z at (9, 2): {g2.predict(np.array([[9, -2]]))}')
--- src/bopt_gmm/gmm/gmm.py
+++ src/bopt_gmm/gmm/gmm.py
@@ -49,7 +49,17 @@ class GMM(object):
     def n_dims(self):
         return self._means.shape[1]
 
-    def get_weights(self, x: np.array, dims=None):
+    def sub_pdf(self, X, component, dims=None):
+        _, n_features = X.shape
+        mean    = self._mu(dims)[component]
+        inv_cov = self._inv_sigma(dims)[component]
+        det_cov = self._sigma_det(dims, dims)[component]
+
+        delta = (X - mean)
+
+        return np.exp(-0.5 * (delta * inv_cov.dot(delta.T).T).sum(axis=1)) / np.sqrt(((2 * np.pi) ** n_features) * det_cov)
+
+    def get_weights(self, X: np.array, dims=None):
         """Calculates the weights of the mixture components for a given point x.
 
         Args:
@@ -66,23 +76,19 @@ class GMM(object):
             np.array: Weights of the individual GMM components as they pertain to x.
         """
         if dims is None:
-            if x.shape[1] < self._means.shape[1]: # Todo: Check fails for 1d inputs
+            if X.shape[1] < self._means.shape[1]: # Todo: Check fails for 1d inputs
                 raise Exception(f'Dimensions for weighting need to '
                                 f'be specified explicitly when given'
                                 f'data is of lower dimensionality.')
                 
             dims = range(self._means.shape[1])
         
-        x = x if x.ndim == 2 else x.reshape((1, x.size))
+        X = X if X.ndim == 2 else X.reshape((1, X.size))
 
-        weights = np.zeros((self._priors.size, x.shape[0]))
-        # Filtered covariances
-        f_cvar  = self._cvar.take(dims, axis=1).take(dims, axis=2)
-        for k in range(self._priors.size):
-            c_mean = self._means[k].take(dims)
-            c_cvar = f_cvar[k]
-            weights[k] = self._priors[k] * multivariate_normal.pdf(x, c_mean, c_cvar)
-        
+        weights = np.zeros((self._priors.size, X.shape[0]))
+        for k in range(self.n_priors):
+            weights[k] = self._priors[k] * self.sub_pdf(X, k, dims)
+
         # Normalized weights
         return weights / (np.sum(weights, axis=0) + sys.float_info.epsilon)
 
@@ -105,37 +111,78 @@ class GMM(object):
             raise Exception(f'Data with {x.shape[1]} dimensions was given, '
                             f'but {len(d_given)} dimensions were specified.')
         
-        d_predicted = [i for i in range(self._means.shape[1]) if i not in d_given]
+        d_predicted = tuple(i for i in range(self._means.shape[1]) if i not in d_given)
 
         # (K, num x)
         weights  = self.get_weights(x, d_given)
         p_mean   = np.zeros((x.shape[0], len(d_predicted)))
         # (given, given, K)
-        f_g_cvar  = self._cvar.take(d_given, axis=1).take(d_given, axis=2)
+        f_g_cvar  = self._sigma(d_given, d_given)
         # (predicted, given, K)
-        f_pg_cvar = self._cvar.take(d_predicted, axis=1).take(d_given, axis=2)
-        for k in range(self._priors.size):
-            # (given, 1)
-            c_g_mean  = self._means[k].take(d_given)
-            # (predicted, 1)
-            c_p_mean  = self._means[k].take(d_predicted)
-            # (given, given)
-            c_g_cvar  = f_g_cvar[k]
-            # (predicted, given)
-            c_pg_cvar = f_pg_cvar[k]
+        f_pg_cvar = self._sigma(d_given, d_predicted)
+        for k, (c_g_mean, c_p_mean, c_g_cvar, c_pg_cvar) in enumerate(zip(self._mu(d_given), 
+                                                                          self._mu(d_predicted), f_g_cvar, f_pg_cvar)):
             #     (pred, K)  (pred, given)  (given, given)               (num_x, given)
             aux_1 = (x - c_g_mean).T
             aux_2 = c_pg_cvar.dot(np.linalg.pinv(c_g_cvar)).dot(aux_1)
             aux   = c_p_mean + aux_2.T
-            p_mean += weights[k] * aux
+            p_mean += (aux.T * weights[k]).T
         return p_mean
+
+    def conditional_pdf(self, X : np.array, d_given, *Ys : np.array):
+        X = X if X.ndim == 2 else x.reshape((1, X.size))
+
+        if len(d_given) != X.shape[1]:
+            raise Exception(f'Data with {X.shape[1]} dimensions was given, '
+                            f'but {len(d_given)} dimensions were specified.')
+
+        if len(Ys) == 0:
+            raise Exception('Need Y points to assess conditional probability at')
+
+        d_predicted = tuple(i for i in range(self._means.shape[1]) if i not in d_given)
+        Y = np.stack(Ys, 1)
+        if max([Y.shape[1] != len(d_predicted) for Y in Ys]):
+            raise Exception(f'Inference points do not match desidered dimensionality {len(d_predicted)} {[Y.shape[1] for Y in Ys]}')
+        
+        if max([Y.shape[0] != X.shape[0] for Y in Ys]):
+            raise Exception(f'Inference points must match number of sample points {X.shape[0]} {[Y.shape[0] for Y in Ys]}')
+
+        weights     = self.get_weights(X, d_given).T
+        g_mean      = self._mu(d_given)
+        p_mean      = self._mu(d_predicted)
+        # (given, given, K)
+        f_g_cvar    = self._sigma(d_given, d_given)
+        # (predicted, given, K)
+        f_pg_cvar   = self._sigma(d_given, d_predicted)
+        # (predicted, predicted, K)
+        f_pp_cvar   = self._sigma(d_predicted, d_predicted)
+        # (given, predicted, K)
+
+        pdfs = np.zeros((Y.shape[0], Y.shape[1]))
+
+        f_g_inv_cvar = self._inv_sigma(d_given)
+        f_gp_cvar    = self._sigma(d_given, d_predicted)
+        f_pg_cvar    = self._sigma(d_predicted, d_given)
+        f_p_cvar     = self._sigma(d_predicted, d_predicted)
+
+        cd_cvar_temp = np.stack([gp_cvar.dot(inv_cvar) for gp_cvar, inv_cvar in zip(f_gp_cvar, f_g_inv_cvar)], 0)
+
+        cd_cvar = np.stack([p_cvar - t_cvar.dot(pg_cvar) for p_cvar, t_cvar, pg_cvar in zip(f_p_cvar, cd_cvar_temp, f_pg_cvar)], 0)
+
+        for k, (x, w, y) in enumerate(zip(X, weights, Y)):
+            g_delta = (x - g_mean)
+            means   = p_mean + np.vstack([t_cvar.dot(d) for t_cvar, d in zip(cd_cvar_temp, g_delta)])
+
+            sub_gmm = GMM(w, means, cd_cvar)
+            pdfs[k] = sub_gmm.pdf(y)
+
+        return pdfs.T
 
     @property
     @lru_cache(1)
     def _cvar_tril_idx(self):
         temp = np.vstack(np.tril_indices(self._cvar.shape[1]))
         return tuple(np.hstack([np.vstack(([i] * temp.shape[1], temp)) for i in range(self.n_priors)]))
-
 
     def update_gaussian(self, priors=None, mu=None, sigma=None):
         """Returns a new GMM updated with the given deltas
@@ -212,27 +259,42 @@ class GMM(object):
 
         return type(self)(self._priors, new_mu, new_sigma)
 
-    def pdf(self, points : np.ndarray):
+    @lru_cache(10)
+    def _mu(self, dims=None):
+        if dims is not None:
+            return self._means[:,dims]
+        return self._means
+
+    @lru_cache(10)
+    def _sigma(self, dims_in=None, dims_out=None):
+        if dims_in is not None:
+            if dims_out is None:
+                return self._cvar[:, :, dims_in]
+            return self._cvar[:, dims_out][:,:,dims_in]
+        if dims_out is not None:
+            return self._cvar[:, dims_out, :]
+        return self._cvar
+
+    @lru_cache(10)
+    def _sigma_det(self, dims_in=None, dims_out=None):
+        sigma = self._sigma(dims_in, dims_out)
+        return np.asarray([np.linalg.det(s) for s in sigma])
+
+    @lru_cache(10)
+    def _inv_sigma(self, dims=None):
+        return np.stack([np.linalg.inv(sigma) for sigma in self._sigma(dims, dims)], 0)
+
+    def pdf(self, points : np.ndarray, dims=None):
         """Calculate the PDF of an array of points in the form (n, dim)
 
         Args:
             points (np.ndarray): Stacked array of points
         """
-        # Inverted covariances (priors, dims, dims)
-        inv_cov = np.stack([np.linalg.inv(sigma) for sigma in self._cvar], 0)
-        # Delta from the means as (priors, dims, points)
-        delta   = np.expand_dims(self._means, -1) - np.expand_dims(points.T, 0)
-        # Should be (priors, dims, points)
-        temp    = np.stack([ic.dot(sd) for ic, sd in zip(inv_cov, delta)], 0)
-        # Dot with the original delta (priors, points)
-        dot     = (delta * temp).sum(axis=1)
-        e       = np.exp(-0.5 * dot)
-        factor  = 1 / ((2 * np.pi)**(np.pi * 0.5) * np.abs(self._cvar)**0.5)
-        # Should be (priors, points)
-        probabilites = factor * e * self._priors
-        # Marginalize over overall probabilities
-        probabilites /= probabilites.sum(axis=0)
-        return probabilites
+        pdf = np.zeros(points.shape[0])
+        for k, w in enumerate(self._priors):
+            pdf += w * self.sub_pdf(points, k, dims)
+
+        return pdf
 
     def pi(self):
         return self._priors.copy()
@@ -265,44 +327,3 @@ class GMM(object):
             np.ndarray: Filtered means (n_priors, |dims|)
         """
         return self._means.copy() if dims is None else self._means.take(dims, axis=1)
-
-
-if __name__=='__main__':
-    class GMM3DPredictZ(GMM):
-        def __init__(self, priors, means, cvar):
-            super().__init__(priors=priors, means=means, cvar=cvar)
-
-        def predict(self, xy):
-            return super().predict(xy, [0, 1])
-
-        def predictX(self, yz):
-            return super().predict(yz, [1, 2])
-
-    g1 = GMM(priors=np.array([1, 1, 1]), 
-             means=np.array([[1, 2, 3]]).T, 
-             cvar=np.array([[[0.1]]]*3))
-    w1 = g1.get_weights(np.array([[1, 2, 3]]).T)
-    print(f'Weights for 1, 2, 3:\n{w1}')
-
-    g2 = GMM3DPredictZ(priors=np.array([1,  1,  1]),
-                       means=np.array([[1,  1,  1],
-                                       [4,  5, -2],
-                                       [9, -2,  4]]),
-                       cvar=np.stack([np.eye(3)]*3, axis=0))
-    
-    w2 = g2.get_weights(np.array([[1,  1,  1],
-                                  [4,  5, -2],
-                                  [9, -2,  4]]))
-    print(f'Weights for 3d case:\n{w2}')
-
-    w3 = g2.get_weights(np.array([[1,  1],
-                                  [4,  5],
-                                  [9, -2]]), dims=[0, 1])
-    print(f'Weights for 3d case without Z:\n{w3}')
-
-    w4 = g2.get_weights(np.array([[6.5, 1.5, 1]]))
-    print(f'3d point perfectly between 2 and 3:\n{w4}')
-
-    p1 = g2.predict(np.array([[4, 5]]))
-    print(f'Prediction for Z at (4, 5): {p1}')
-    print(f'Prediction for Z at (9, 2): {g2.predict(np.array([[9, -2]]))}')
