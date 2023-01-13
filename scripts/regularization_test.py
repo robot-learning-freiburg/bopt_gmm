cbc7ad0b4b555190854b719db315fec756290c71
Adrian Roefer <aroefer@uni-freiburg.de>
2023-01-13 17:08:39 +0100

    Added implementation of regularization functions

---
+++ scripts/regularization_test.py
@@ -0,0 +1,51 @@
+import glob
+import numpy  as np
+import pandas as pd
+
+from argparse import ArgumentParser
+from pathlib  import Path
+
+import bopt_gmm.bopt.regularization as reg
+
+from bopt_gmm.utils import unpack_trajectories
+from bopt_gmm.gmm   import GMMCart3DForce, rollout, GMMCart3D
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Calculates the regularization values for a models and trajectories in a folder.')
+    parser.add_argument('path', help='Directory to process.')
+    parser.add_argument('trajectories', nargs='+', help='Trajectories used as sample for regularization.')
+    args = parser.parse_args()
+
+    pattern = f'{args.path}/gmm_*.npy'
+
+    positions = [pos for _, _, _, pos in unpack_trajectories(args.trajectories, 
+                                                             [np.load(t, allow_pickle=True) for t in args.trajectories], 
+                                                             ['position', 'force'])]
+    # positions = np.vstack(positions)
+
+    gmms = []
+
+    for p in glob.glob(pattern):
+        p = Path(p)
+        
+        if p.name == 'gmm_base.npy':
+            base_gmm = GMMCart3D.load_model(p)
+        else:
+            gmms.append((p, GMMCart3D.load_model(p)))
+        
+    f_regs = [reg.f_joint_prob, reg.f_mean_prob, reg.f_kl, reg.f_jsd, reg.f_dot]
+
+    values = []
+
+    gmms.insert(0, (Path('gmm_base.npy'), base_gmm))
+
+    positions = np.vstack([rollout(gmms[0][1], p[0][:len(gmms[0][1].state_dim)], True) for p in positions])
+
+    for gmm_path, gmm in gmms:
+        values.append([f(gmm, base_gmm, positions) for f in f_regs])
+    
+    print(''.join([' ' * 15] + [f'{f.__name__:>15}' for f in f_regs]))
+    for (gp, _), values in zip(gmms, values):
+        print(''.join([f'{gp.name:>14} '] + [f' {v:>14.5f}' for v in values]))
+
+    
---
+++ src/bopt_gmm/bopt/regularization.py
@@ -0,0 +1,76 @@
+import numpy as np
+import sys
+
+from dataclasses import dataclass
+from typing      import Callable, Any
+
+def f_zero(model_new, model_base, points):
+    return 0
+
+
+def f_joint_prob(model_new, model_base, points):
+    pos   = points[:, model_base.state_dim]
+    base_inf = model_base.predict(pos, model_base.state_dim)
+    new_inf  = model_new.predict(pos, model_base.state_dim)
+    cond_prob_base, cond_prob_new = model_base.conditional_pdf(pos, model_base.state_dim, base_inf, new_inf)
+
+    # joint_prob = np.log(cond_prob_new) / np.log(cond_prob_base)
+    # conditional  = np.nan_to_num(joint_prob / pos_prob)
+    # conditional /= conditional.max() + sys.float_info.epsilon
+    return np.log(cond_prob_new).sum() / np.log(cond_prob_base).sum()
+
+
+def f_mean_prob(model_new, model_base, points):
+    pos   = points[:, model_base.state_dim]
+    base_inf = model_base.predict(pos, model_base.state_dim)
+    new_inf  = model_new.predict(pos, model_base.state_dim)
+    cond_prob_base, cond_prob_new = model_base.conditional_pdf(pos, model_base.state_dim, base_inf, new_inf)
+
+    return cond_prob_new.mean() / cond_prob_base.mean()
+
+
+def f_kl(model_new, model_base, points):
+    p_mb = model_base.pdf(points, tuple(range(points.shape[1])))
+    p_mn = model_new.pdf(points, tuple(range(points.shape[1])))
+    return np.sum(np.where(p_mb != 0, p_mb * np.log(p_mb / p_mn), 0))
+
+
+def f_jsd(model_new, model_base, points):
+    p_mb = model_base.pdf(points, tuple(range(points.shape[1])))
+    p_mn = model_new.pdf(points, tuple(range(points.shape[1])))
+    return 0.5 * (np.sum(np.where(p_mb != 0, p_mb * np.log(p_mb / p_mn), 0)) + 
+                  np.sum(np.where(p_mn != 0, p_mn * np.log(p_mn / p_mb), 0)))
+
+
+def f_dot(model_new, model_base, points):
+    i = tuple(range(points.shape[1]))
+    inv_mb = model_base.predict(points, i)
+    b_norm = np.sqrt((inv_mb ** 2).sum(axis=1))
+    inv_mb = (inv_mb.T / b_norm).T
+    inv_mn = (model_new.predict(points, i).T / b_norm).T
+
+    return np.nan_to_num((inv_mb * inv_mn).sum(axis=1), nan=1.0).mean()
+
+
+def gen_regularizer(cfg):
+    if cfg is None or cfg.f == 'zero':
+        return f_zero
+    
+    if cfg.f == 'p_joint':
+        def f_reg(model_new, model_base, points):
+            return f_joint_prob(model_new, model_base, points) * cfg.b
+    elif cfg.f == 'p_mean':
+        def f_reg(model_new, model_base, points):
+            return f_mean_prob(model_new, model_base, points) * cfg.b
+    elif cfg.f == 'kl':
+        def f_reg(model_new, model_base, points):
+            return f_kl(model_new, model_base, points) * cfg.b
+    elif cfg.f == 'jsd':
+        def f_reg(model_new, model_base, points):
+            return f_jsd(model_new, model_base, points) * cfg.b
+    elif cfg.f == 'dot':
+        def f_reg(model_new, model_base, points):
+            return np.exp(-np.abs(f_kl(model_new, model_base, points))) * cfg.b
+
+    return f_reg
+
