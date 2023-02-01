Added correct observation scaling to models

src/bopt_gmm/gmm/instances.py
@@ -49,8 +49,8 @@ class GMMCart3DForce(GMM):
     def predict(self, obs_dict, dims=GIVEN, full=False):
         if type(obs_dict) == dict:
             obs_dict = np.hstack((obs_dict['position'], obs_dict['force']))
-        scale = np.isin(dims, self.GIVEN[-3:]).astype(float) * self._force_scale
-        return super().predict(obs_dict * scale, dims)[:,:3] if not full else super().predict(obs_dict, dims)
+        scale = np.isin(dims, self.GIVEN[-3:], invert=True) + np.isin(dims, self.GIVEN[-3:]).astype(float) * self._force_scale
+        return super().predict(obs_dict * scale, dims)[:,:3] if not full else super().predict(obs_dict * scale, dims)
 
     def mu_pos(self):
         return self.mu([0, 1, 2])
@@ -87,19 +87,24 @@ class GMMCart3DForce(GMM):
     def conditional_pdf(self, X: np.array, d_given, *Ys: np.array):
         d_predicted = [x for x in set(range(len(self.GIVEN) * 2)) if x not in d_given]
         
-        scale_x = np.isin(d_given, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(self._force_scale)
-        scale_y = np.isin(d_predicted, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(self._force_scale)
+        scale_x = np.isin(d_given, self.GIVEN[-3:] + self.prediction_dim[-3:], invert=True) + np.isin(d_given, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(float) * self._force_scale
+        scale_y = np.isin(d_predicted, self.GIVEN[-3:] + self.prediction_dim[-3:], invert=True) + np.isin(d_predicted, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(float) * self._force_scale
         
         return super().conditional_pdf(X * scale_x, d_given, *[y * scale_y for y in Ys])
 
     def pdf(self, points : np.ndarray, dims=None):
-        scale  = np.isin(dims, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(float) * self._force_scale
+        scale  = np.isin(dims, self.GIVEN[-3:] + self.prediction_dim[-3:], invert=True) + np.isin(dims, self.GIVEN[-3:] + self.prediction_dim[-3:]).astype(float) * self._force_scale
         return super().pdf(points * scale, dims)
 
     def _custom_data(self):
         d = super()._custom_data()
         d.update({'force_scale': self._force_scale})
         return d
+
+    def update_gaussian(self, priors=None, mu=None, sigma=None):
+        new_model = super().update_gaussian(priors, mu, sigma)
+        new_model._force_scale = self._force_scale
+        return new_model
 
 add_gmm_model(GMMCart3DForce)
 
@@ -119,4 +124,4 @@ GMM_TYPES = {'position': GMMCart3D,
 
 
 def load_gmm(gmm_config):
-    return GMM_TYPES[gmm_config.type].load_model(gmm_config.model)
+    return GMM.load_model(gmm_config.model)