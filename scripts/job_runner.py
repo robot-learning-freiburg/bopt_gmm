698f337ab9a7004d4899c099f154c4825e766ab9
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2023-02-20 13:53:30 +0100

    Added ability to load commands from file

--- scripts/job_runner.py
+++ scripts/job_runner.py
@@ -1,5 +1,7 @@
 import sys
 
+from pathlib import Path
+
 from bopt_gmm.utils import JobRunner
 
 if __name__ == '__main__':
@@ -11,7 +13,11 @@ if __name__ == '__main__':
     except ValueError as e:
         nproc = 10
 
-    runner = JobRunner([j.split(' ') for j in jobs], nproc)
+    if Path(jobs[0]).exists() and Path(jobs[0]).is_file():
+        with open(jobs[0], 'r') as f:
+            jobs = f.readlines()
+
+    runner = JobRunner([[a for a in j.strip().split(' ') if a != ''] for j in jobs], nproc)
     print(f'Running {len(jobs)}...')
     runner.run()
     print('Done')
