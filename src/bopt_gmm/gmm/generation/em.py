68c2ae5da5d221f65a71f6cf256119e48d2280a1
Adrian Röfer <aroefer@uni-bremen.de>
2023-03-10 18:38:50 +0100

    Added GMR as GMM fitting solution

--- src/bopt_gmm/gmm/generation/em.py
+++ src/bopt_gmm/gmm/generation/em.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 from scipy.stats import multivariate_normal
-from sklearn.mixture import BayesianGaussianMixture
+
+USE_SKLEARN = False
+
+if USE_SKLEARN:
+    from sklearn.mixture import BayesianGaussianMixture
+else:
+    import gmr
 
 from bopt_gmm.gmm   import GMM
 from bopt_gmm.utils import unpack_transition_traj, \
                            calculate_trajectory_velocities
+
 
 def gmm_fit_em(n_components, points, gmm_type=GMM, max_iter=100, tol=0.01, n_init=1, prior_gmm=None, model_kwargs={}):
     """EM-Algorithm for fitting a GMM to data
@@ -19,51 +26,20 @@ def gmm_fit_em(n_components, points, gmm_type=GMM, max_iter=100, tol=0.01, n_ini
     prior_means   = None if prior_gmm is None else prior_gmm.mu()
     prior_cvars   = None if prior_gmm is None else prior_gmm.sigma()
 
-    bgmm = BayesianGaussianMixture(n_components=n_components,
-                                   max_iter=max_iter,
-                                   random_state=np.random.RandomState(0),
-                                   tol=tol,
-                                   n_init=n_init if prior_gmm is None else 1,
-                                   warm_start=prior_gmm is not None)
-    if prior_gmm is not None:
-        bgmm.weights_ = prior_gmm.pi()
-        bgmm.means_   = prior_gmm.mu()
-        bgmm.covariances_ = prior_gmm.sigma()
-        bgmm.converged_ = True
-        bgmm.lower_bound_ = -np.inf
-        bgmm._check_parameters(points)
-        # bgmm.degrees_of_freedom_prior_ = prior_means.shape[1]
-        bgmm._estimate_weights(bgmm.weights_)
-        bgmm._estimate_means(bgmm.weights_, bgmm.means_)
-        bgmm._estimate_precisions(bgmm.weights_, bgmm.means_, bgmm.covariances_)
-    bgmm.fit(points)
 
-    # means = points[np.random.choice(points.shape[0], n_components)]
-    # gmm   = GMM(n_components, means)
+    if USE_SKLEARN:
+        bgmm = BayesianGaussianMixture(n_components=n_components,
+                                       max_iter=max_iter,
+                                       random_state=np.random.RandomState(0),
+                                       tol=tol,
+                                       n_init=n_init if prior_gmm is None else 1)
+        bgmm.fit(points)
+        return gmm_type(bgmm.weights_, bgmm.means_, bgmm.covariances_, **model_kwargs)
+    else:
+        bgmm = gmr.GMM(n_components, prior_weights, prior_means, prior_cvars)
 
-    # # probability of points belonging to components (priors, points)
-    # probs        = gmm.pdf(points)
-    # prior_mass   = probs.sum(axis=1)
-    # # New prior weights
-    # prior_weight = prior_mass / prior_mass.sum()
-    # # Points scaled per prior (priors, dims, points)
-    # weighted_points = np.expand_dims(probs, axis=-1) * np.expand_dims(points.T, axis=0)
-    # new_means       = (1 / prior_weight) * weighted_points.sum(axis=2)
-
-    # # Deltas (priors, dims, points)
-    # new_delta = np.expand_dims(new_means, -1) - np.expand_dims(points.T, 0)
-
-    # new_cvars = np.zeros(gmm._cvar.shape)
-
-    # for x, delta in enumerate(new_delta):
-    #     sigmas       = np.stack([d.dot(d.T) for d in np.expand_dims(delta.T, -1)]) * probs[x]
-    #     new_cvars[x] = sigmas.sum(axis=0)
-
-    # new_gmm = GMM(prior_weight, new_means, new_cvars)
-
-    # cov = nearestPD(bgmm.covariances_) if not isPD(bgmm.covariances_) else bgmm.covariances_
-
-    return gmm_type(bgmm.weights_, bgmm.means_, bgmm.covariances_, **model_kwargs)
+        bgmm.from_samples(points, n_iter=max_iter, init_params='kmeans++')
+        return gmm_type(bgmm.priors, bgmm.means, bgmm.covariances, **model_kwargs)
 
 
 def em_gmm_generator(gmm_type, n_priors, max_iter, tol, n_init, data_order=['position', 'force', 'torque']):
